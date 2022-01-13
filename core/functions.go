9a88ebeba73dc5c179ce23698840f1e901c9c870
wojuzhuhai <wojuzhuhai@qq.com>
2022-01-13 15:46:38 +0800

    Update functions.go

--- core/functions.go
+++ core/functions.go
@@ -1,1 +1,21 @@
+package core
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+const (
+	DateFormat = "2006-01-02 15:02"
+)
+
+func init() {
+
+}
+
+func md5V(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
